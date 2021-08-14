ddc4c21c911bae89fa3fb58d8dcba763c37041fe
Yash Agrawal <yashagrawal81930@gmail.com>
2021-08-14 12:31:58 +0530

    finished

---
+++ server.js
@@ -0,0 +1,29 @@
+const io = require('socket.io')(8000, {
+    cors: {
+        origin: '*',
+    }
+});
+
+const users = {};
+
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        users[socket.id] = name;
+        console.log('new user joined', name);
+        socket.broadcast.emit('user-joined', name);
+
+    })
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', {
+            message: message,
+            name: users[socket.id]
+        });
+
+    })
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    })
+})
